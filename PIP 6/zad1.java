1f085e5918d288b50a2de280459adbeac399795a
Mateusz Meges <115637944+matis731@users.noreply.github.com>
2023-01-10 12:27:34 +0100

    Update zad1.java

--- PIP 6/zad1.java
+++ PIP 6/zad1.java
@@ -28,12 +28,12 @@ public class zad1 {
     }
 
     public static int[] sortArray(int[] sortedArr) {
-        int[] sortedArray = sortedArr.clone(); // kopia tablicy
+        int[] sortedArray = sortedArr.clone();
 
         for (int i = 0; i < sortedArray.length - 1; i++) {
             for (int j = 0; j < sortedArray.length - i - 1; j++) {
                 if (sortedArray[j] > sortedArray[j + 1]) {
-                    // zamiana miejscami
+
                     int temp = sortedArray[j];
                     sortedArray[j] = sortedArray[j + 1];
                     sortedArray[j + 1] = temp;
